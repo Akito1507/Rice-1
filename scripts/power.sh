86f1fcc80aece847bd95a99a643f4a53fc89b7b4
Akito1507 <akira.takahashi156@proton.me>
2026-06-14 20:41:03 -0700

    Update power.sh

--- scripts/power.sh
+++ scripts/power.sh
@@ -1,1 +1,24 @@
+#!/usr/bin/env bash
 
+option=$(printf "Shutdown\0icon\x1f$HOME/.config/backgrounds/assets/power-logo.png\nLogout\0icon\x1f$HOME/.config/backgrounds/assets/logout-logo.png\nReboot\0icon\x1f$HOME/.config/backgrounds/assets/restart-logo.png\nReboot UEFI\0icon\x1f$HOME/.config/backgrounds/assets/restart-logo.png\nLock\0icon\x1f$HOME/.config/backgrounds/assets/lock-logo.png\nSuspend\0icon\x1f$HOME/.config/backgrounds/assets/suspend-logo.png" | rofi -dmenu -p "Power Menu" -theme "~/.config/rofi/type-2/style-7-config")
+
+case "$option" in
+    "Shutdown")
+        systemctl poweroff
+        ;;
+    "Logout")
+       command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()' 
+        ;;
+    "Reboot")
+        systemctl reboot
+        ;;
+    "Reboot UEFI")
+        systemctl reboot --firmware-setup
+        ;;
+    "Lock")
+        hyprlock
+        ;;
+    "Suspend")
+        systemctl suspend
+        ;;
+esac
