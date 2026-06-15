ab5cf7de383ba0c41cb8eecb4d0ff4f78b6f03f2
Akito1507 <akira.takahashi156@proton.me>
2026-06-14 20:39:30 -0700

    Update config-menu.sh

--- scripts/config-menu.sh
+++ scripts/config-menu.sh
@@ -1,18 +1,24 @@
-#!/usr/bin/env fish
+#!/usr/bin/env bash
 
-set option (printf "Hyprland\0icon\x1f/home/akito/.config/backgrounds/assets/hyprland-logo.png\nKitty\0icon\x1f/home/akito/.config/backgrounds/assets/kitty-logo.png\nWaybar\0icon\x1f/home/akito/.config/backgrounds/assets/archlinux-logo.png\nSwayNC\0icon\x1f/home/akito/.config/backgrounds/assets/wayland-logo.png\nRofi\0icon\x1f/home/akito/.config/backgrounds/assets/rofi-logo.jpg\nYazi\0icon\x1f/home/akito/.config/backgrounds/assets/yazi-logo.png" | rofi -dmenu -p "Edit Config" -theme "~/.config/rofi/type-2/style-7-config")
+option=$(printf "Hyprland\0icon\x1f$HOME/.config/backgrounds/assets/hyprland-logo.png\nKitty\0icon\x1f$HOME/.config/backgrounds/assets/kitty-logo.png\nWaybar\0icon\x1f$HOME/.config/backgrounds/assets/archlinux-logo.png\nSwayNC\0icon\x1f$HOME/.config/backgrounds/assets/wayland-logo.png\nRofi\0icon\x1f$HOME/.config/backgrounds/assets/rofi-logo.jpg\nYazi\0icon\x1f$HOME/.config/backgrounds/assets/yazi-logo.png" | rofi -dmenu -p "Edit Config" -theme "~/.config/rofi/type-2/style-7-config")
 
-switch $option
-    case "Hyprland"
-        kitty --hold yazi "/home/akito/.config/hypr/modules"
-    case "Kitty"
-        kitty --hold nvim "/home/akito/.config/kitty/kitty.conf"
-    case "Waybar"
-        kitty --hold nvim "/home/akito/.config/waybar/config.jsonc"
-    case "SwayNC"
-        kitty --hold nvim "/home/akito/.config/swaync/config.json"
-    case "Rofi"
-        kitty --hold nvim "/home/akito/.config/rofi/type-2/style-7.rasi"
-    case "Yazi"
-        kitty --hold nvim "/home/akito/.config/yazi/yazi.toml"
-end
+case "$option" in
+    "Hyprland")
+        kitty --hold yazi "$HOME/.config/hypr/modules"
+        ;;
+    "Kitty")
+        kitty --hold nvim "$HOME/.config/kitty/kitty.conf"
+        ;;
+    "Waybar")
+        kitty --hold nvim "$HOME/.config/waybar/config.jsonc"
+        ;;
+    "SwayNC")
+        kitty --hold nvim "$HOME/.config/swaync/config.json"
+        ;;
+    "Rofi")
+        kitty --hold nvim "$HOME/.config/rofi/type-2/style-7.rasi"
+        ;;
+    "Yazi")
+        kitty --hold nvim "$HOME/.config/yazi/yazi.toml"
+        ;;
+esac
